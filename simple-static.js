0b1411d38f121c062ae55136443311c98d2eff3c
Mark McKelvy <mmckelvy@gmail.com>
2016-04-25 13:33:11 -0700

    some re-work

--- simple-static.js
+++ simple-static.js
@@ -1,47 +1,40 @@
 'use strict';
 
-var fs = require('fs');
-var crypto = require('crypto');
-var path = require('path');
-
-var mime = require('mime');
-
-var headers = require('./headers');
-var createFriendlyError = require('./error').createFriendlyError;
-
-var rootDir = process.cwd();
+const fs = require('fs');
+const crypto = require('crypto');
+const path = require('path');
+const mime = require('mime');
 
 /**
 * statFile() Checks if a file exists and if so, returns file info.
 *
-* @param: filePath <String>
-* @param: callback <Function>
+* @param: filePath {String}
+* @param: callback {Function}
 */
 function statFile(filePath, callback) {
   fs.stat(filePath, function(err, stats) {
 
     // Resource does not exist
     if (err && (err.code === 'ENOENT' || err.code === 'ENOTDIR')) {
-      err.fnCall = 'statFile';
-      console.log(err);
+      const error = new Error('Resource does not exist');
+      error.httpCode = 404;
 
-      var friendlyError = createFriendlyError(404, 'Not Found');
-      callback(friendlyError);
-
-    // Server error
-    } else if (err) {
-      err.fnCall = 'statFile';
-      console.log(err);
-
-      var friendlyError = createFriendlyError(500, 'Internal Server Error');
-      callback(friendlyError);
+      callback(error);
 
     // Not a file
     } else if (!stats.isFile()) {
-      var friendlyError = createFriendlyError(400, 'Bad Request');
-      callback(friendlyError);
+      const error = new Error('Resource is not a file');
+      error.httpCode = 400;
 
-    // Get the stats
+      callback(error);
+
+    // Server error
+    } else if (err) {
+      const error = new Error('Server error');
+
+      callback(error);
+
+    // Success. Get the stats.
     } else {
       callback(null, stats);
     }
@@ -50,8 +43,8 @@ function statFile(filePath, callback) {
 
 /**
 * createServerEtag()
-* @param: inode <Number> The file inode number
-* @param: mTime <Date> JavaScript date object (returned from mtime in fs.stat call).
+* @param: inode {Number} The file inode number
+* @param: mTime {Date} JavaScript date object (returned from mtime in fs.stat call).
 *
 * returns: an etag for use in HTTP headers.
 */
@@ -63,7 +56,14 @@ function createServerEtag(inode, mTime) {
   return crypto.createHash('md5').update(str).digest('hex');
 }
 
-// Check for cached resources.
+/**
+* should304() - Checks for cached resources
+*
+* @param: req {Object} Node request object
+* @param: stats {Object} Result of a Node fs.stat call.
+*
+* returns {Boolean}
+*/
 function should304(req, stats) {
   var clientEtag = req.headers['if-none-match'];
 
@@ -75,38 +75,56 @@ function should304(req, stats) {
   return true;
 }
 
-// Serve the file
-function serveFile(req, res, stats) {
-  var staticPath = path.join(rootDir, req.url);
-  var stream = fs.createReadStream(path.join(rootDir, req.url));
+/**
+* serveFile() - Serves a static file
+*
+* @param: req {Object} Node request object.
+* @param: res {Object} Node response object.
+* @param: stats {Object} Results from a Node fs.stat call.
+* @param: filePath {String} The full file path for the static resource.
+* @param: cache {String} Cache-Control headers. Defaults to private, 24hrs.
+*
+* returns {void} Sends a response to the client.
+*/
+function serveFile(req, res, stats, filePath, cache) {
+  var stream = fs.createReadStream(filePath);
 
   stream.on('error', function(err) {
-    err.fnCall = 'serveFile';
-    console.log(err);
+    const error = new Error('Error reading file');
 
-    var friendlyError = createFriendlyError(500, 'Internal Server Error');
-
-    res.writeHead(500, headers.json);
-    res.end(JSON.stringify(friendlyError));
+    res.writeHead(500, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(error);
   });
 
   // Verify this won't get called in an error event
   res.writeHead(200, {
-    'Content-Type': mime.lookup(staticPath),
+    'Content-Type': mime.lookup(filePath),
     'ETag': createServerEtag(stats.ino, stats.mtime),
-    'Cache-Control': 'private, max-age=86400'
+    'Cache-Control': cache ? cache : 'private, max-age=86400'
   });
 
   stream.pipe(res);
 }
 
 // PUBLIC
-function serveStaticContent(req, res) {
-  var staticPath = path.join(rootDir, req.url);
+/**
+* serveStaticContent() - Main, publicly exposed function.
+*
+* @param: req {Object} Node request object.
+* @param: res {Object} Node response object.
+* @param: options {Object} Possible options are as follows:
+*
+* options.rootDir {String} Root directory. Defaults to process.cwd()
+*
+* returns {void} Sends a response to the client.
+*/
+function serveStaticContent(req, res, options) {
+  const rootDir = options.rootDir ? options.rootDir : process.cwd();
+  const filePath = path.join(rootDir, req.url);
 
-  statFile(staticPath, function(err, stats) {
+  statFile(filePath, function(err, stats) {
     if (err) {
-      res.writeHead(err.name, headers.json);
+      res.writeHead(err.httpCode, {'Content-Type': 'application/json'});
       res.end(JSON.stringify(err));
 
     } else if (should304(req, stats)) {
@@ -114,7 +132,7 @@ function serveStaticContent(req, res) {
       res.end();
 
     } else {
-      serveFile(req, res, stats);
+      serveFile(req, res, stats, filePath, options.cache);
     }
   });
 }
