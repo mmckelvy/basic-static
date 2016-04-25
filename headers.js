ca0f370a9caee8a74081a5977bcad38c74e151aa
Mark McKelvy <mmckelvy@gmail.com>
2016-04-25 12:08:00 -0700

    more package.json updates

---
+++ headers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  json: {'Content-Type': 'application/json'},
+  html: {'Content-Type': 'text/html'},
+  css: {'Content-Type': 'text/css'},
+  js: {'Content-Type': 'application/javascript'},
+  text: {'Content-Type': 'text/plain'},
+  urlencoded: {'Content-Type': 'application/x-www-form-urlencoded'}
+};
