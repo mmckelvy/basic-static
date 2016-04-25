2e09408ad28244655a22811ecac744bf24629959
Mark McKelvy <mmckelvy@gmail.com>
2016-04-25 16:24:22 -0700

    some initial unit tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,61 @@
 'use strict';
 
-require('tape');
+const http = require('http');
+const url = require('url');
+const test = require('tape');
 
+const simpleStatic = require('../lib/simple-static');
+
+test('Should return a 200 response for a requested file', function(t) {
+
+  // Set up the test server.
+  const server = http.createServer(function(req, res) {
+    simpleStatic(req, res, {rootDir: __dirname});
+  });
+
+  server.listen(3000, function() {
+    console.log('Server started');
+  });
+
+  // Make a request to the server.
+  const options = {
+    protocol: 'http:',
+    hots: 'localhost',
+    port: 3000,
+    method: 'GET',
+    path: '/testfiles/main.js'
+  };
+
+  http.get(options, function(res) {
+    t.equal(res.statusCode, 200, 'Should return a 200');
+    server.close();
+    t.end();
+  });
+});
+
+test('Should return a 404 for a file that does not exist', function(t) {
+
+  // Set up the test server.
+  const server = http.createServer(function(req, res) {
+    simpleStatic(req, res, {rootDir: __dirname});
+  });
+
+  server.listen(3000, function() {
+    console.log('Server started');
+  });
+
+  // Make a request to the server.
+  const options = {
+    protocol: 'http:',
+    hots: 'localhost',
+    port: 3000,
+    method: 'GET',
+    path: '/testfiles/not-found.js'
+  };
+
+  http.get(options, function(res) {
+    t.equal(res.statusCode, 404, 'Should return a 404');
+    server.close();
+    t.end();
+  });
+});
